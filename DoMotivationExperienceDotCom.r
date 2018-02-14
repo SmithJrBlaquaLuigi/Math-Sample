36836ba22dd08fc58843a93ec6ea2865ace190e9
Damon V. Smith Jr <damon0959@gmail.com>
2018-02-14 03:44:19 -0500

    Sweet! Please look at my Math Sample I made!

---
+++ DoMotivationExperienceDotCom.r
@@ -0,0 +1,36 @@
+## Copyright 2018 SmithJrBlaquaLuigi
+## This file is a small open source code that you can use for R programming practice.
+#################################################################################################
+Smith <- Blaqua <-  6
+Smith
+Blaqua
+'This is a sample math I made on the code that complies those numbers when mutiplying them correct.'
+class(Blaqua * Smith)
+numeric(6)
+12 * 2
+'Square roots is in use when do triple times instead of double as a common factor of a number.'
+36 + 96
+132 ^ 6  
+x ^ y
+if (6 * 96 (7+ 5))
+  2 ^ 96
+Blaqua * x
+Smith ^ 
+numeric(12)
+DominosPizza <- 1
+Pepperoni <- 8
+class(DominosPizza * Pepperoni)
+{
+(DominosPizza * Pepperoni * numeric(8 * 1 ^ 8))
+  {  
+numeric(8)
+8 ^ 1
+12 ^ 8
+DominosPizza ^ Pepperoni
+
+1 ^ 8 + 8 + 1
+
+c <- ( 1 + 8 ^ 8 ^ 1)
+
+ }
+}
