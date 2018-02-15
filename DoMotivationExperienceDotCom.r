d1ea1af2f80d257c3a1f905a6579437e0006d99f
Damon V. Smith Jr <damon0959@gmail.com>
2018-02-14 20:55:31 -0500

    Decimal and Float explanation.

--- DoMotivationExperienceDotCom.r
+++ DoMotivationExperienceDotCom.r
@@ -47,14 +47,41 @@ We can dividing them to get a floating point number. '
   factor (0.24462890625 /8)
   factor(0.03057861328125 / 8)
   factor(0.00382232666015625 / 8)
+}  
+  if (0.00382232666015625 > 1001 / 7)'cannot divide that floating point number by 7'
+    break
   
+ (DominosPizza / Pepperoni )
+  1001/125
   
-  'Now we can understand why the floating point number on the right -> "0.000477790832519531" cannot be divided 
-by 2 any longer, but if you continue to do so, a current number you divide
-will no longer show the floating point number since it shows the negative number and alphabet on the console
-(Of course, the cacluation gets confused and makes it stay that way.)
+  'As you can see, the factor of those numbers were divided by 125 or 125.25. Please look at 8.00, 4.00, 2.00, 1.00, 0.50 and 0.25 below, that explains how much money do you have. 
+  Although, those numbers you put it in like 8.008, 4.004, etc. as I mentioned earlier are not common factors.'
+  
+  'Begins with hundredths before moving up to thousandths, etc.'
+  factor(8.008 /2)
+  factor(4.004 / 2)
+  factor (2.002 / 2)
+  factor (1.001 / 2)
+  factor (0.5005 / 2)
+  'adds another ten thousandth below.'
+  factor (0.25025 /2 )
+  'adds another hundred thousandth below.' 
+  factor (0.125125 / 2)
+  
+  ' 0.125125 versus 125.25'
+  
+  'Maybe I can explain a little better. Its funny when you divide 1001 by 8, It brings up the decimal number first,
+  then continue to divide by the same number to bring up the floating point number.
+  
+  1.9xxxxx are floats.
+  15.xxxxx are decimals. (sometimes are floats. Not 100% sure yet.)
+
+^ Again, the number: 0.125125 is a hundred-thousandth decimal place, and 125.25 is the tenth decimal place. 
+not a float unless you divide the same number over again to get a floating point number.
+
+This is different btw. It cannot be 0.125.25 in tenth decimal place because it has two decimals in the number which is not permitted in that type.
+You will recieve a syntax error if you use that number with two decimals to divide the same number.
+
 
   '
-  
  }
-}
